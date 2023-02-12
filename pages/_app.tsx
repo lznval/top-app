ebfe61d05891cd86046074ae5b0ea7c2a5a2d0e9
lznval <lizunova.252@yandex.ru>
2023-02-12 15:48:19 +0400

    add Head component, add document file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>MyTop - наш лучший топ</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
