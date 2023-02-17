73b180f30d0e2082626f7e65b246138c65a6ec3c
lznval <lizunova.252@yandex.ru>
2023-02-17 10:22:25 +0400

    add P component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag } from "../components";
+import { Button, Htag, P } from "../components";
 
 export default function Home(): JSX.Element {
 	return (
@@ -6,6 +6,8 @@ export default function Home(): JSX.Element {
 			<Htag tag="h1">Привет</Htag>
 			<Button arrow="right" appearance="ghost">Кнопка</Button>
 			<Button arrow="down" appearance="primary">Кнопка</Button>
+			<P>Какой-то текст</P>
+			<P size='l'>Какой-то текст с размером</P>
 		</>
 	);
 }
