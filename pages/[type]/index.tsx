f3ed056fc2eaf7ed722ba9e5be30a38749bcb1e4
lznval <lizunova.252@yandex.ru>
2023-05-24 12:53:02 +0400

    много нового. изменили архитектуру, добавили комп.

---
+++ pages/[type]/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { withLayout } from '../../layout/Layout';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import { firstLevelMenu } from '../../helpers/helpers';
+import { ParsedUrlQuery } from 'querystring';
+import axios from 'axios';
+import { MenuItem } from '../../interfaces/menu.interface';
+
+const Courses = ({firstCategory}: CoursesProps) => {
+  return (
+    <div>Courses: {firstCategory}</div>
+  );
+};
+
+export default withLayout(Courses);
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	return {
+		paths: firstLevelMenu.map(m => '/' + m.route),
+		fallback: true
+	};
+};
+export const getStaticProps: GetStaticProps<CoursesProps> = async ({ params }: GetStaticPropsContext<ParsedUrlQuery>) => {
+	if (!params) {
+		return {
+			notFound: true
+		};
+	}
+	const firstCategoryItem = firstLevelMenu.find(m => m.route == params.type);
+	if (!firstCategoryItem) {
+		return {
+			notFound: true
+		};
+	}
+	const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory: firstCategoryItem.id
+	});
+	return {
+		props: {
+			menu,
+			firstCategory: firstCategoryItem.id
+		}
+	};
+};
+
+interface CoursesProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
---
+++ pages/search.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { withLayout } from "../layout/Layout";
+
+const SearchPage = () => {
+  return <div>search</div>;
+};
+
+export default withLayout(SearchPage);
