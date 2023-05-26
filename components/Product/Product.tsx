64886769e0524012271569a4446af1b64a336280
lznval <lizunova.252@yandex.ru>
2023-05-26 12:32:09 +0400

    add declOfNum func, add features block

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -6,7 +6,7 @@ import { Card } from '../Card/Card';
 import { Rating } from '../Rating/Rating';
 import { Tag } from '../Tag/Tag';
 import { Button } from '../Button/Button';
-import { priceRu } from '../../helpers/helpers';
+import { declOfNum, priceRu } from '../../helpers/helpers';
 import { Divider } from '../Divider/Divider';
 
 export const Product = ({product, className, ...props}: ProductProps): JSX.Element => {
@@ -27,10 +27,18 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
             <div className={styles.tags}>{product.categories.map(c => <Tag className={styles.category} key={c} color='ghost'>{c}</Tag>)}</div>
             <div className={styles.priceTitle}>цена</div>
             <div className={styles.creditTitle}>кредит</div>
-            <div className={styles.ratingTitle}>{product.reviewCount} отзывов</div>
+            <div className={styles.ratingTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
             <Divider className={styles.hr} />
             <div className={styles.description}>{product.description}</div>
-            <div className={styles.feature}>Фичи</div>
+            <div className={styles.features}>
+                {product.characteristics.map(c => (
+                    <div className={styles.characteristics} key={c.name}>
+                        <span className={styles.characteristicsName}>{c.name}</span>
+                        <span className={styles.characteristicsDots}></span>
+                        <span className={styles.characteristicsValue}>{c.value}</span>
+                    </div>
+                ))}
+            </div>
             <div className={styles.advBlock}>
                 {product.advantages && 
                     <div className={styles.advantages}>
