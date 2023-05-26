f31a7e771cb2cbb50373924ad6f80841c2309502
lznval <lizunova.252@yandex.ru>
2023-05-26 14:00:35 +0400

    product component layout, adaprive

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -61,7 +61,7 @@ export const Product = ({product, className, ...props}: ProductProps): JSX.Eleme
                     </div> 
                 }                               
             </div>
-            <Divider className={styles.hr} />
+            <Divider className={cn(styles.hr, styles.hr2)} />
             <div className={styles.actions}>
                 <Button appearance='primary'>Узнать подробнее</Button>
                 <Button appearance='ghost' arrow='right' className={styles.reviewButton}>Читать отзывы</Button>
