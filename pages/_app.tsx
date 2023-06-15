1120c4cc3808cbc413445ecc08caac0844ca1008
lznval <lizunova.252@yandex.ru>
2023-06-15 12:51:00 +0400

    add husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,31 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/globals.css';
+import ym, {YMInitializer} from 'react-yandex-metrika';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+	router.events.on('routeChangeComplete', (url: string) => {
+		if (typeof window !== 'undefined') {
+			ym('hit', url);
+		}
+	});
 	return (
 		<>
 			<Head>
 				<title>MyTop - наш лучший топ</title>
 				<link rel="icon" href="/favicon.ico" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://mc.yandex.ru" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
 				<link href="https://fonts.googleapis.com/css2?family=Cabin&family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet" />
 				<meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
 				<meta property='og:locale' content='ru_RU' />
 			</Head>
+			<YMInitializer
+				accounts={[]}
+				options={{webvisor: true, defer: true}}
+				version='2'
+			/>
 			<Component {...pageProps} />
 		</>
 	);
