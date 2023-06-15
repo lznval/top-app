2ac57582926eabaa045005f4d0209f2d5ae92864
lznval <lizunova.252@yandex.ru>
2023-06-15 11:42:40 +0400

    add meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
 	return (
 		<>
 			<Head>
@@ -11,6 +11,8 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
 				<link href="https://fonts.googleapis.com/css2?family=Cabin&family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet" />
+				<meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+				<meta property='og:locale' content='ru_RU' />
 			</Head>
 			<Component {...pageProps} />
 		</>
